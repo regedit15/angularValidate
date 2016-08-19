2ff9e8fa7aeb101e38129b48719cbccf877d0e49
regedit15 <001regedit15@gmail.com>
2016-08-19 09:18:52 -0300

    se corrige validacion

--- validaciones/www/js/controllers/mainController.js
+++ validaciones/www/js/controllers/mainController.js
@@ -5,16 +5,16 @@ app.controller('mainController', [ '$scope', '$state',
        $scope.cantidad;
 
        $scope.successPrecio = function() {
-           return $scope.form.cantidad.$valid && $scope.form.cantidad.$touched;
+           return $scope.form.cantidad.$valid && $scope.form.cantidad.$dirty;
        }
 
        $scope.errorPrecio = function() {
-           return !$scope.form.cantidad.$valid && $scope.form.cantidad.$touched;
+           return !$scope.form.cantidad.$valid && $scope.form.cantidad.$dirty;
        }
 
        $scope.validarCantidadMaxima = function(cantidad) {
             //este nombre, "cantidadInvalida" tiene que ser el mismo que esta en messages.html
-            $scope.cantidadInvalida = ((cantidad >= 1000) && $scope.form.cantidad.$touched);
+            $scope.form.cantidad.$error.cantidadInvalida = ((cantidad >= 1000) && $scope.form.cantidad.$dirty);
         }
 
         $scope.volver = function(cantidad) {
