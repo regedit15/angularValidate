2de706b880892db59343edfe916b57704a32236f
regedit15 <001regedit15@gmail.com>
2016-08-19 16:42:15 -0300

    se corrige directiva

--- validaciones/www/js/controllers/mainController.js
+++ validaciones/www/js/controllers/mainController.js
@@ -1,39 +1,18 @@
 app.controller('mainController', [ '$scope', '$state',
 
-   function($scope, $state) {
+function($scope, $state) {
 
+	$scope.guardar = function() {
+		alert('submit!');
+	}
 
-       $scope.numero;
-       $scope.cantidad;
-       $scope.hayError;
+	$scope.volver = function(cantidad) {
+		if ($state.current.name === 'inicio') {
+			$state.go('acercaDe');
+		} else {
+			$state.go('inicio');
+		}
 
+	}
 
-       $scope.successPrecio = function() {
-           return $scope.form.cantidad.$valid && $scope.form.cantidad.$dirty;
-       }
-
-       $scope.errorPrecio = function() {
-           return ($scope.form.cantidad.$invalid && $scope.form.cantidad.$dirty) || $scope.cantidadInvalida;
-       }
-
-       $scope.validarCantidadMaxima = function(cantidad) {
-            //este nombre, "cantidadInvalida" tiene que ser el mismo que esta en messages.html
-            $scope.cantidadInvalida = cantidad >= 1000;
-        }
-
-
-        $scope.guardar = function() {
-          alert('submit!');
-         }
-
-        $scope.volver = function(cantidad) {
-         if($state.current.name === 'inicio'){
-            $state.go('acercaDe');
-         }else{
-            $state.go('inicio');
-         }
-
-
-     }
-
- }]);
+} ]);
--- validaciones/www/js/controllers/validacionDirective.js
+++ validaciones/www/js/controllers/validacionDirective.js
@@ -1,70 +1,75 @@
-app.directive('validacion', [ 
+app.directive('validacion', [
 
 function() {
-  return {
-    templateUrl : 'validacionDirective.html',
-    restrict : 'E',
-    replace : true,
-    scope : {
-      'valor' : '=',
-      'minlength' : '=',
-      'maxlength' : '=',
-      'pattern' : '@',
-      'error' : '=',
-    },
+	return {
+		templateUrl : 'validacionDirective.html',
+		restrict : 'E',
+		replace : true,
+		scope : {
+			'valor' : '=',
+			'minlength' : '=',
+			'maxlength' : '=',
+			'pattern' : '@',
+			'error' : '=',
+		},
 
-    link : function(scope, element, attrs) {
+		link : function(scope, element, attrs) {
 
-      // scope.saludo = 'Hola';
-      // // para usarlo dentro del html de la directiva
-      // scope.Constantes = Constantes;
+			scope.mostrarMinlength;
+			scope.mostrarMaxlength;
+			scope.mostrarPattern;
 
-      // scope.saludarClick = function(conferencia) {
-      //   console.log('Holaaaaaaa ' + conferencia.ciudadanoId.nombre)
-      // };
+			function MinLengthException() {
+			}
 
-      // scope.minlength;
-      // scope.maxlength;
-      // scope.pattern;
+			function MaxLengthException() {
+			}
 
-      scope.validarValor = function(funcion) {
-             if (scope.valor != null && scope.valor != '') {
-                return funcion();
-             }else{
-                scope.error = undefined;
-             }
-      };
+			function PatternException() {
+			}
 
-      scope.mostrarMinlength = function() {
-             return scope.validarValor(function() {
-                scope.error = scope.valor.length < scope.minlength;
-                 return scope.error;
-             });
-      };
+			scope.$watch('valor', function(newValue, oldValue) {
 
-       scope.mostrarMaxlength = function() {
-        return scope.validarValor(function() {
-                 scope.error = scope.valor.length > scope.maxlength;
-                 return scope.error;
-             });
-      };
+				var validar = function() {
+					if (!new RegExp(scope.pattern).test(scope.valor)) {
+						scope.mostrarPattern = true;
+						scope.mostrarMinlength = false;
+						scope.mostrarMaxlength = false;
+						throw new PatternException();
+					}
 
-      scope.mostrarPattern = function() {
-        return scope.validarValor(function() {
+					if (scope.valor.length < scope.minlength) {
+						scope.mostrarMinlength = true;
+						scope.mostrarPattern = false;
+						scope.mostrarMaxlength = false;
+						throw new MinLengthException();
+					}
 
-                if (scope.error == false) {
-                 scope.error = !new RegExp(scope.pattern).test(scope.valor);
-                 return scope.error;   
-                }
-                 
-             });
-      };
+					if (scope.valor.length > scope.maxlength) {
+						scope.mostrarMaxlength = true;
+						scope.mostrarMinlength = false;
+						scope.mostrarPattern = false;
+						throw new MaxLengthException();
+					}
+				};
 
+				if (scope.valor != null && scope.valor != '') {
 
+					try {
+						validar();
+						scope.error = 1;
+						scope.mostrarMaxlength = false;
+						scope.mostrarMinlength = false;
+						scope.mostrarPattern = false;
 
+					} catch (e) {
+						scope.error = -1;
+					}
 
-
-
-    }
-  }
+				} else {
+					scope.error = 0;
+				}
+			});
+		}
+	}
 } ]);
